bb726aec57b4cd4dc3382cb9a8f14bf5d2e5c0f7
Jose Noriega <josernoriega7@gmail.com>
2023-10-25 22:41:48 +0200

    Changes made in the database module

--- database.py
+++ database.py
@@ -1,3 +1,45 @@
 import mysql.connector
 
-# New data base
+config = {
+    'host':'localhost',
+    'user':'root',
+    'password':'root',
+    'database':'MyDatabase',
+}
+
+newTable = {"""
+            CREATE TABLE items 
+            (id INTEGER PRIMARY KEY AUTO_INCREMENT,
+            Name VARCHAR (50),
+            Price INTEGER (20),)
+            """}
+
+def createDb():
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+    cursor.execute("CREATE DATABASE MyDatabase")
+    cursor.close()
+
+def createNewTable():
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+    cursor.execute('''CREATE TABLE items 
+                    (id INTEGER PRIMARY KEY AUTO_INCREMENT,
+                    Name VARCHAR (50),
+                    Price INTEGER (20)) 
+                   ''')
+    cursor.close()
+
+def connectDatabase():
+    db = mysql.connector.connect(**config)
+    return db
+
+def queryDb():
+    db = mysql.connector.connect(**config)
+    cursor = db.cursor()
+    cursor.execute("SHOW TABLES")
+    
+    for x in cursor:
+        print(x)
+
+queryDb()
